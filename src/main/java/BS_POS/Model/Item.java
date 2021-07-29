a27f970881a7e60c672f41fb329d6b9e75ae7697
jorget93 <jorgetapia0293@gmail.com>
2021-07-29 13:18:41 -0400

    created item class model

---
+++ src/main/java/BS_POS/Model/Item.java
@@ -0,0 +1,31 @@
+package BS_POS.Model;
+
+public class Item {
+    private String name = "";
+    private Double price = 0.0;
+    private Integer itemCount = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(Integer itemCount) {
+        this.itemCount = itemCount;
+    }
+}
