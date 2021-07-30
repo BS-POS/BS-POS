42dae56f2b7ee7f4c2353bdaf626ff9c5c39abbb
jorget93 <jorgetapia0293@gmail.com>
2021-07-30 14:16:13 -0400

    added fields, getters, setters to customerCheck class

--- src/main/java/BS_POS/Model/CustomerCheck.java
+++ src/main/java/BS_POS/Model/CustomerCheck.java
@@ -1,9 +1,7 @@
 package BS_POS.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class CustomerCheck {
@@ -11,5 +9,40 @@ public class CustomerCheck {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+    private String tableNumber;
+    @OneToMany
+    private Set<Item> itemLog;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(String tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public Set<Item> getItemLog() {
+        return itemLog;
+    }
+
+    public void setItemLog(Set<Item> itemLog) {
+        this.itemLog = itemLog;
+    }
 }
