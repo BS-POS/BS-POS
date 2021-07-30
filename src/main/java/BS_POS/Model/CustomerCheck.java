3382d961b81435eee9f154c336fb73ed3943dbe7
jorget93 <jorgetapia0293@gmail.com>
2021-07-30 14:11:14 -0400

    changed customerCheck class name for more clarification

--- src/main/java/BS_POS/Model/CustomerCheck.java
+++ src/main/java/BS_POS/Model/CustomerCheck.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Check {
+public class CustomerCheck {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
