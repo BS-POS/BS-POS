5b4a107ede8bde060ec2c930c854cb47b748c0c6
jorget93 <jorgetapia0293@gmail.com>
2021-08-02 15:54:01 -0400

    changed the Repositories to interfaces and extended CrudRepo

--- src/main/java/BS_POS/Repositories/ItemRepository.java
+++ src/main/java/BS_POS/Repositories/ItemRepository.java
@@ -1,4 +1,9 @@
 package BS_POS.Repositories;
 
-public class ItemRepository {
+import BS_POS.Model.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends CrudRepository <Item, Long> {
 }
--- src/main/java/BS_POS/Repositories/ModifierRepository.java
+++ src/main/java/BS_POS/Repositories/ModifierRepository.java
@@ -1,4 +1,9 @@
 package BS_POS.Repositories;
 
-public class ModifierRepository {
+import BS_POS.Model.Modifier;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModifierRepository extends CrudRepository <Modifier, Long> {
 }
