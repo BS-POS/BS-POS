3cd362fd976ba98ac1cfe59b319b548ad2922ea0
Jorge Tapia <jorgetapia@Jorges-MacBook-Pro.local>
2021-08-07 12:12:48 -0400

    added a few methods to the customerCheckService class

--- src/main/java/BS_POS/Services/CustomerCheckService.java
+++ src/main/java/BS_POS/Services/CustomerCheckService.java
@@ -1,5 +1,6 @@
 package BS_POS.Services;
 
+import BS_POS.Model.CustomerCheck;
 import BS_POS.Repositories.CustomerCheckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,4 +13,17 @@ public class CustomerCheckService {
     public CustomerCheckService(CustomerCheckRepository customerCheckRepository) {
         this.customerCheckRepository = customerCheckRepository;
     }
+
+    public Iterable<CustomerCheck> indexAllCustomerChecks() {
+        return customerCheckRepository.findAll();
+    }
+
+    public CustomerCheck showCustomerCheck(Long id){
+        return customerCheckRepository.findById(id).get();
+    }
+
+    public CustomerCheck createCustomerCheck(CustomerCheck newCheck){
+        return customerCheckRepository.save(newCheck);
+    }
+
 }
