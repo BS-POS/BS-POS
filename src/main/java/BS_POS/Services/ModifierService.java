9701889c96a0e2907f1e1dd894aaf034617a0674
Jorge Tapia <jorgetapia@Jorges-MacBook-Pro.local>
2021-08-07 12:41:11 -0400

    added anotations to modifier service class

--- src/main/java/BS_POS/Services/ModifierService.java
+++ src/main/java/BS_POS/Services/ModifierService.java
@@ -1,4 +1,16 @@
 package BS_POS.Services;
 
+import BS_POS.Repositories.ModifierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ModifierService {
+
+    private final ModifierRepository modifierRepository;
+
+    @Autowired
+    public ModifierService(ModifierRepository modifierRepository) {
+        this.modifierRepository = modifierRepository;
+    }
 }
