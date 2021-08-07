77d7a06572911c8b58bb909011f2a59d54032df0
Jorge Tapia <jorgetapia@Jorges-MacBook-Pro.local>
2021-08-07 12:04:25 -0400

    anotated CustomerCheckService with beans and added constructor

--- src/main/java/BS_POS/Services/CustomerCheckService.java
+++ src/main/java/BS_POS/Services/CustomerCheckService.java
@@ -1,4 +1,15 @@
 package BS_POS.Services;
 
+import BS_POS.Repositories.CustomerCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerCheckService {
+    private final CustomerCheckRepository customerCheckRepository;
+
+    @Autowired
+    public CustomerCheckService(CustomerCheckRepository customerCheckRepository) {
+        this.customerCheckRepository = customerCheckRepository;
+    }
 }
