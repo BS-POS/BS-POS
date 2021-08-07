87d02ad018e25494766e7aeeded61f1b93e8e41c
Jorge Tapia <jorgetapia@Jorges-MacBook-Pro.local>
2021-08-07 12:17:10 -0400

    added anotations and constructor to item service class

--- src/main/java/BS_POS/Services/ItemService.java
+++ src/main/java/BS_POS/Services/ItemService.java
@@ -1,4 +1,20 @@
 package BS_POS.Services;
 
+import BS_POS.Model.Item;
+import BS_POS.Repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+
+
 }
